16e96fcf55b322788adf1a3f091e1b7cb569cefc
felibunnyy <felibunbun13@gmail.com>
2024-06-01 13:58:02 +0800

    add previous edits

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_05_27_090038) do
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'articles', force: :cascade do |t|
+    t.string 'title'
+    t.text 'description'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
